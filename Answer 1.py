44b4aab2a7eff112219c70b6dfe3b388531d2dc2
vinoth199 <70647867+vinoth199@users.noreply.github.com>
2020-09-05 08:30:59 +0530

    Assignment day 3

---
+++ Answer 1.py
@@ -0,0 +1,71 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 19,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      " Enter the altitude: 1000\n",
+      " Safe to land \n"
+     ]
+    }
+   ],
+   "source": [
+    "num = input(\" Enter the altitude: \")\n",
+    "m = int(num)\n",
+    "if m < 1000 or m == 1000:\n",
+    "        print(\" Safe to land \")\n",
+    "elif m > 1000 and m < 5000:\n",
+    "        print(\" come down to 1000ft\")\n",
+    "elif m > 5000:\n",
+    "        print(\" Turn around and try later\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
